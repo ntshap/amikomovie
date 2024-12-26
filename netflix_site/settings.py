eae3f174ad1574234e877a458b41d7f84b89948c
Natasha <natasharondonuwu@gmail.com>
2024-12-26 22:27:09 +0700

    update

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -85,13 +85,17 @@ WSGI_APPLICATION = 'netflix_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'amikomovie_database',
-        'USER': 'postgres',
+        'NAME': 'amikomovie',
+        'USER': 'amikomovie',
         'PASSWORD': 'Cloud_1234',
-        'HOST': 'localhost',
+        'HOST': 'amikomovie.postgres.database.azure.com',
         'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
     }
 }
+
 
 
 # Password validation
